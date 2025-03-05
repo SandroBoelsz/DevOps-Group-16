9813b8b8da44962c98ba549edd23285c393a7be0
Menno Konijn <menno.konijn@usmedia.nl>
2025-03-05 12:58:12 +0100

    FIX: Changed run.py to work with docker compose up

--- workflow-data-replication-api/run.py
+++ workflow-data-replication-api/run.py
@@ -1,7 +1,6 @@
 from app import init_app
 
 app = init_app()
-if app is None:
-    print("Failed to initialize the app. Exiting.")
-else:
-    app.run(host='0.0.0.0', port=5000, debug=True)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)  # Only run if executed directly
