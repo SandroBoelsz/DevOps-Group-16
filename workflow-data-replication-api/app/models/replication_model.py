f9166e13dae01bb2b065f5958f1724c843caf3bb
Menno Konijn <menno.konijn@usmedia.nl>
2025-03-11 16:02:49 +0100

    feature/us-32: Implemented LRU algorithm and able to fetch size of file

--- workflow-data-replication-api/app/models/replication_model.py
+++ workflow-data-replication-api/app/models/replication_model.py
@@ -3,7 +3,7 @@ from flask_restx import fields
 
 def get_replication_model(replication_ns):
     return replication_ns.model("ReplicationModel", {
-        "endpoint": fields.String(required=True, description="Endpoint of the MinIO server"),
+        "minioUrl": fields.String(required=True, description="Endpoint of the MinIO server"),
         "bucket": fields.String(required=True, description="Name of the bucket"),
         "filename": fields.String(required=True, description="Name of the file"),
     })
