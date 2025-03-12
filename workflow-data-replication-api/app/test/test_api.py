391864dcbfab61afae829eac4ded96bebde5249f
Thomas <35685453+thomasdecactus@users.noreply.github.com>
2025-03-12 19:47:53 +0100

    3 basic tests

--- workflow-data-replication-api/app/test/test_api.py
+++ workflow-data-replication-api/app/test/test_api.py
@@ -11,6 +11,12 @@ class TestPytest(unittest.TestCase):
 
 class TestAPI(unittest.TestCase):
     BASE_URL = "http://127.0.0.1:5001"
+    ENDPOINT = "/workflow-data-replication/trigger"
+    REQUEST_PAYLOAD = {
+        "minioUrl": "https://scruffy.lab.uvalight.net:9000",
+        "bucket": "naa-vre-user-data",
+        "filename": "s.boelsz@gmail.com/testen.txt",
+    }
 
     def test_trigger_method_not_allowed(self):
         url = f"{self.BASE_URL}/workflow-data-replication/trigger"
@@ -19,6 +25,16 @@ class TestAPI(unittest.TestCase):
         expected_message = {"message": "The method is not allowed for the requested URL."}
 
         self.assertEqual(response.status_code, expected_status)
+        self.assertEqual(response.json(), expected_message)
+
+    def test_trigger_post_request_with_correct_payload(self):
+        url = f"{self.BASE_URL}{self.ENDPOINT}"
+        response = requests.post(url, json=self.REQUEST_PAYLOAD)
+        expected_status = 200  # We expect method not allowed
+        expected_message = {"message": "File already present in the Dutch S3 bucket"}  
+
+        # Check if the response status is as expected (adjust if needed)
+        self.assertEqual(response.status_code, 200)  
         self.assertEqual(response.json(), expected_message)
 
 if __name__ == "__main__":
